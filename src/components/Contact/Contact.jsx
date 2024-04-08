d5a6906380391dabcf3484aa8d2dfa6e0d701ff0
Diego Vilhlava <diegorocha450@yahoo.com>
2024-04-08 08:04:16 -0300

    update contact form

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -15,7 +15,7 @@ const Contact = () => {
       setResult("Enviando....");
       const formData = new FormData(event.target);
   
-      formData.append("access_key", "3539f688-efd6-421b-ab59-06eb237cee27");
+      formData.append("access_key", import.meta.env.VITE_API_KEY);
   
       const response = await fetch("https://api.web3forms.com/submit", {
         method: "POST",
